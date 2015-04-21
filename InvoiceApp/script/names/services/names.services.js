9e2ac5fd51f626fba05abb9477e97486731f5b37
unknown <bongarde@gmail.com>
2015-04-22 01:47:47 +0530

    UI level changes using bootstrap added table to show customers, added form to add new customer.

--- InvoiceApp/script/names/services/names.services.js
+++ InvoiceApp/script/names/services/names.services.js
@@ -1,11 +1,8 @@
 angular.module('names').factory("nameService",[function(){
     return({
         items:[
-            'Name 1',
-            'Name 2'
-        ],
-        addName: function(name){
-            this.items.push(name);
-        }
+            {id:1, name:'Hege',  addr:'Pege', cost:'150' },
+            {id:2, name:'Kim',   addr:'Pim', cost:'150' }
+        ],test:"abcd"
     })
 }]);
--- InvoiceApp/script/partials/partials.module.js
+++ InvoiceApp/script/partials/partials.module.js
@@ -2,10 +2,15 @@ angular.module('partials',['ui.router']).config(function($stateProvider,$urlRout
     $urlRouterProvider.otherwise("/state1/list");//default state
     $stateProvider
         .state('state1',{
-            url:"/state1",
-            templateUrl:"script/partials/about.html"
+            url:"/list",
+            templateUrl:"script/partials/customerList.html",
+            controller:function($scope){
+                $scope.items=["Item1","Item2","Item3"]
+            }
+
         }).state('state2',{
             url:"/state2",
-            templateUrl:"script/partials/contact.html"
+            templateUrl:"script/partials/invoiceList.html"
         })  //url can be kept blank, it is used to show changes url in browser.
+
 });
