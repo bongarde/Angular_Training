35dbf56a7055943e30dceebfcbece11b9e24e6d7
unknown <bongarde@gmail.com>
2015-04-22 09:59:33 +0530

    nested json objects

--- InvoiceApp/script/names/services/names.services.js
+++ InvoiceApp/script/names/services/names.services.js
@@ -1,8 +1,8 @@
 angular.module('names').factory("nameService",[function(){
     return({
         items:[
-            {id:1, name:'Hege',  addr:'Pege', cost:'150' },
-            {id:2, name:'Kim',   addr:'Pim', cost:'150' }
+            {id:1, name:'Hege',  addr:'Pege', cost:'150', cars:[{make:'BMW'},{make:'Merc'}] },
+            {id:2, name:'Kim',   addr:'Pim', cost:'150', cars:[{make:'BMW1'},{make:'Merc1'}] }
         ],addCustomer: function(id,name,addr){
             var i = parseInt(this.items.length)+parseInt("1");
             var customer = {id:parseInt(this.items.length)+parseInt("1"),name:name,addr:addr,cost:'0'};
